b8ab9c34dec04322d37f7716ffcc8987524662b8
diogomsantos <diogosantos@easyclick.pt>
2018-12-16 22:45:40 +0000

    touch listener implemented

--- app/src/main/java/com/example/diogosantos/reversisec/GameActivity.java
+++ app/src/main/java/com/example/diogosantos/reversisec/GameActivity.java
@@ -3,7 +3,10 @@ package com.example.diogosantos.reversisec;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.example.diogosantos.reversisec.logic.Game;
 
@@ -35,5 +38,20 @@ public class GameActivity extends AppCompatActivity {
 
         gridView = findViewById(R.id.playBoard);
         gridView.setAdapter(game);
+
+        gridView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                int currentXPos = (int) event.getX();
+                int currentYPos = (int) event.getY();
+
+                int position = gridView.pointToPosition(currentXPos, currentYPos);
+
+                Toast.makeText(GameActivity.this, Integer.toString(position), Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
     }
 }
