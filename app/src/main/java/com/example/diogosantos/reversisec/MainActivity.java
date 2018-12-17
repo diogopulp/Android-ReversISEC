cd94a5273db70b3f1e1d3b1f68196ff633268214
marcelo_cortesao <marcelo.cortesao@gmail.com>
2018-12-17 23:47:22 +0000

    navigation drawer first implementation

--- app/src/main/java/com/example/diogosantos/reversisec/MainActivity.java
+++ app/src/main/java/com/example/diogosantos/reversisec/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.diogosantos.reversisec;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,10 +14,19 @@ public class MainActivity extends AppCompatActivity {
     Button btn_game;
     Button btn_take_photo;
 
+    private DrawerLayout nDrawerLayout;
+    private ActionBarDrawerToggle nToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //menu action drawer
+        nDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        nToggle = new ActionBarDrawerToggle(this,nDrawerLayout,R.string.drawer_open,R.string.drawer_close);
+        nDrawerLayout.addDrawerListener(nToggle);
+        nToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         btn_take_photo = (Button)findViewById(R.id.btn_take_photo);
         btn_take_photo.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +45,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(nToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
